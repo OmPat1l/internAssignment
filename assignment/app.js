e37a2f6aad4068146b7b0c02851680be57919072
Om Patil <omajitsinhpatil@gmail.com>
2023-04-12 14:34:06 +0530

    login 1

--- assignment/app.js
+++ assignment/app.js
@@ -16,7 +16,7 @@ const colref = db.collection("users");
 app.use(express.json());
 let userLogged = 0;
 //signing up
-app.post("/signup/", async (req, res) => {
+app.post("/signup", async (req, res) => {
   let buff = req.body;
   let name = buff.name;
   let email = buff.email;
@@ -47,6 +47,31 @@ app.post("/signup/", async (req, res) => {
       console.error(error);
       return res.status(500).json({ error: "Failed to add user" });
     }
+  } catch (error) {
+    return res.status(500).json({ error: "Server failed" });
+  }
+});
+//logging in
+app.post("/login", async (req, res) => {
+  let buff = req.body;
+  let name = buff.name;
+  let email = buff.email;
+  let password = buff.password;
+  if (!name || !password || !email) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+  try {
+    const usersSnapshot = await colref.get();
+    const users = [];
+    usersSnapshot.forEach((doc) => {
+      users.push(doc.data());
+    });
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].email == email) {
+        //login activity
+      }
+    }
+    return res.status(200).json({ message: "account does not exist" });
   } catch (error) {
     return res.status(500).json({ error: "Server failed" });
   }
